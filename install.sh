a6907308830a81039528eb4223ac0daf6086c066
Marc Partensky <marc.partensky@gmail.com>
2025-07-15 17:13:08 +0200

    5120 mib for efi partition

--- install.sh
+++ install.sh
@@ -83,10 +83,10 @@ partition_disk () {
 
  parted --script --align=optimal $disk -- \
      mklabel gpt \
-     mkpart EFI 1MiB 511MiB \
-     mkpart BIOS 511MiB 512MiB \
-     mkpart swap 512MiB $((SWAPSIZE + 512))MiB \
-     mkpart nixos $((SWAPSIZE + 512))MiB -$((RESERVE + 150))MiB \
+     mkpart EFI 1MiB 5119MiB \
+     mkpart BIOS 5119MiB 5120MiB \
+     mkpart swap 5120MiB $((SWAPSIZE + 5120))MiB \
+     mkpart nixos $((SWAPSIZE + 5120))MiB -$((RESERVE + 150))MiB \
      mkpart boot -$((RESERVE + 150))MiB -$((RESERVE + 1))MiB \
      set 1 esp on \
      set 2 bios_grub on \
